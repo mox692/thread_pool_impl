9d6866e3166b64043815ccb40ac5527c42313d28
mox692 <55653825+mox692@users.noreply.github.com>
2021-05-10 22:20:24 +0900

    initial commitgo build -x

---
+++ README.md
@@ -0,0 +1,1 @@
+## basic thread pool implementation in C.
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+int main() {
+    
+}
